0af6cf3a642e6d3ba1c4fcaaed8efb29b3d29f37
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-12-17 10:53:21 -0500

    Initial commit

---
+++ be-spaceless.ts
@@ -0,0 +1,43 @@
+import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
+import {register} from 'be-hive/register.js';
+import {BeSpacelessVirtualProps, BeSpacelessActions, BeSpacelessProps} from './types';
+
+export class BeSpacelessController implements BeSpacelessActions{
+    #target!: Element;
+    #targetObserver!: ResizeObserver;
+    intro(proxy: Element & BeSpacelessVirtualProps, target: Element, beDecorProps: BeDecoratedProps): void{
+        this.#target = target;
+        this.#targetObserver = new ResizeObserver(entries => {
+            for(const entry of entries){
+                console.log(entry);
+            }
+        });
+    }
+
+    finale(proxy: Element & BeSpacelessVirtualProps, target: Element, beDecorProps: BeDecoratedProps): void{
+        this.#targetObserver.disconnect();
+    }
+
+}
+
+export interface BeSpacelessController extends BeSpacelessProps{}
+
+const tagName = 'be-spaceless';
+
+const ifWantsToBe = 'spaceless';
+
+const upgrade = '*';
+
+define<BeSpacelessProps & BeDecoratedProps<BeSpacelessProps, BeSpacelessActions>, BeSpacelessActions>({
+    config:{
+        tagName,
+        propDefaults:{
+            upgrade,
+            ifWantsToBe,
+            virtualProps: [],
+            intro: 'intro',
+            finale: 'finale'
+        },
+
+    }
+});
--- types.d.ts
+++ types.d.ts
@@ -1,2 +1,14 @@
 import {BeDecoratedProps} from 'be-decorated/types';
 
+export interface BeSpacelessVirtualProps{
+
+}
+
+export interface BeSpacelessProps extends BeSpacelessVirtualProps{
+    proxy: Element & BeSpacelessVirtualProps;
+}
+
+export interface BeSpacelessActions{
+    intro(proxy: Element & BeSpacelessVirtualProps, target: Element, beDecorProps: BeDecoratedProps): void;
+    finale(proxy: Element & BeSpacelessVirtualProps, target: Element, beDecorProps: BeDecoratedProps): void;
+}
